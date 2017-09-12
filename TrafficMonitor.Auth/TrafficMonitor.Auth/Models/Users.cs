e2256e25dd14acc1c03161c2f4abfd1f46887075
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2017-09-12 10:54:25 +0200

    Setup server with configuration files containing test oauth

---
+++ TrafficMonitor.Auth/TrafficMonitor.Auth/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace TrafficMonitor.Auth.Models
+{
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "admin",
+                    Password = "password",
+                    Claims = new List<Claim> {
+                        new Claim(JwtClaimTypes.Email, "jeppe@sam.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
--- TrafficMonitor.Auth/TrafficMonitor.Auth/Startup.cs
+++ TrafficMonitor.Auth/TrafficMonitor.Auth/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TrafficMonitor.Auth.Models;
 
 namespace TrafficMonitor.Auth
 {
@@ -16,6 +17,12 @@ namespace TrafficMonitor.Auth
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+            .AddInMemoryClients(Clients.Get())
+            .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+            .AddInMemoryApiResources(Resources.GetApiResources())
+            .AddTestUsers(Users.Get())
+            .AddTemporarySigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +35,7 @@ namespace TrafficMonitor.Auth
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
